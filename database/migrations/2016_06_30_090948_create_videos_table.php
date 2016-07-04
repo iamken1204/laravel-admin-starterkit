f0d0e31f00341899886a1066b645fa02d70903ff
Kettan <iamken1204@gmail.com>
2016-07-04 11:01:30 +0800

    [add] migrations of video series

--- database/migrations/2016_06_30_090948_create_videos_table.php
+++ database/migrations/2016_06_30_090948_create_videos_table.php
@@ -12,7 +12,49 @@ class CreateVideosTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('videos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('youtube_code', 16)->unique();
+            $table->string('title', 15)->index();
+            $table->text('intro');
+            $table->string('author', 15)->index();
+            // The second argument for integer() is the flag for autoincrement
+            // and the third is for unsigned.
+            $table->integer('likes', false, true)->index();
+            $table->integer('theme_id', false, true)->index();
+            $table->string('thumb_url');
+            $table->enum('in_activity', [0, 1])->default(0);
+            $table->text('activity');
+            $table->string('keyword')->index();
+            $table->integer('sort', false, true)->index();
+            $table->integer('created_at', false, true)->length(10);
+            $table->integer('updated_at', false, true)->length(10);
+        });
+
+        Schema::create('videos_carousel', function(Blueprint $table) {
+            $table->increments('id');
+            // The second argument for integer() is the flag for autoincrement
+            // and the third is for unsigned.
+            $table->integer('video_id', false, true)->index();
+            $table->string('title', 15);
+            $table->enum('status', [0, 1])->default(1)->index();
+            $table->integer('started_at', false, true)->length(10)->index();
+            $table->integer('ended_at', false, true)->length(10)->index();
+            $table->integer('created_at', false, true)->length(10)->index();
+            $table->integer('updated_at', false, true)->length(10);
+        });
+        Schema::create('videos_recommend', function(Blueprint $table) {
+            $table->increments('id');
+            // The second argument for integer() is the flag for autoincrement
+            // and the third is for unsigned.
+            $table->integer('video_id', false, true)->length(10)->index();
+            $table->string('title', 15);
+            $table->enum('status', [0, 1])->default(1)->index();
+            $table->integer('started_at', false, true)->length(10)->index();
+            $table->integer('ended_at', false, true)->length(10)->index();
+            $table->integer('created_at', false, true)->length(10)->index();
+            $table->integer('updated_at', false, true)->length(10);
+        });
     }
 
     /**
@@ -22,6 +64,7 @@ class CreateVideosTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('videos');
+        Schema::drop('videos_carousel');
     }
 }
