1c95a163f6433e70ba0875fb3da088e594a3886f
Kettan <iamken1204@gmail.com>
2016-07-06 15:19:25 +0800

    [layout] base view of admin

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -11,13 +11,100 @@
     @yield('head-styles')
     @yield('head-scripts')
 </head>
-<body>
+<body class="hold-transition skin-purple sidebar-mini">
 
-    @yield('content')
+    <div class="wrapper">
+        <!-- Main Header -->
+        <header class="main-header">
+
+          <!-- Logo -->
+          <a href="#" class="logo">
+            <!-- mini logo for sidebar mini 50x50 pixels -->
+            <span class="logo-mini"><b>kids</b></span>
+            <!-- logo for regular state and mobile devices -->
+            <span class="logo-lg"><b>kids</b> movie</span>
+          </a>
+
+          <!-- Header Navbar -->
+          <nav class="navbar navbar-static-top" role="navigation">
+            <!-- Sidebar toggle button-->
+            <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+              <span class="sr-only">Toggle navigation</span>
+            </a>
+            <!-- Navbar Right Menu -->
+            <div class="navbar-custom-menu">
+              <ul class="nav navbar-nav">
+                <!-- User Account Menu -->
+                <li class="dropdown user user-menu">
+                  <!-- Menu Toggle Button -->
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                    <!-- The user image in the navbar-->
+                    <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+                    <!-- hidden-xs hides the username on small devices so only the image appears. -->
+                    <span class="hidden-xs">Alexander Pierce</span>
+                  </a>
+                  <ul class="dropdown-menu">
+                    <!-- The user image in the menu -->
+                    <li class="user-header">
+                      <p>
+                        Alexander Pierce - Web Developer
+                      </p>
+                    </li>
+                    <!-- Menu Footer-->
+                    <li class="user-footer">
+                      <div class="pull-right">
+                        <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                      </div>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </header>
+
+        <!-- Left side column. contains the logo and sidebar -->
+        <aside class="main-sidebar">
+
+          <!-- sidebar: style can be found in sidebar.less -->
+          <section class="sidebar">
+
+            <!-- Sidebar Menu -->
+            <ul class="sidebar-menu">
+              <li class="header">HEADER</li>
+              <!-- Optionally, you can add icons to the links -->
+              <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
+              <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
+              <li class="treeview">
+                <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                  <li><a href="#">Link in level 2</a></li>
+                  <li><a href="#">Link in level 2</a></li>
+                </ul>
+              </li>
+            </ul>
+            <!-- /.sidebar-menu -->
+          </section>
+          <!-- /.sidebar -->
+        </aside>
+
+        <div class="content-wrapper">
+            @yield('content')
+        </div>
+
+        <!-- Main Footer -->
+        <footer class="main-footer">
+            <!-- To the right -->
+            <div class="pull-right hidden-xs">
+                Anything you want
+            </div>
+            <!-- Default to the left -->
+            <strong>yam &copy; 2016 <a href="#">kids</a>.</strong>
+        </footer>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/g/jquery@2.2.0,bootstrap@3.3.6"></script>
     <script src="/assets/admin/dist/app.min.js"></script>
     @yield('endbody-scripts')
-
 </body>
 </html>
